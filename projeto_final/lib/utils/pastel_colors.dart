2e844869bf4a1cf1c300818814e69f1889924986
DiegoDefSan <defsan35@gmail.com>
2023-12-09 10:18:00 -0300

    feat: pastel colors map

---
+++ projeto_final/lib/utils/pastel_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final Map<String, Color> pastelColors = {
+  "green": const Color(0xFFB2DFDB),
+  "orange": const Color(0xFFFFCCBC),
+  "yellow": const Color(0xFFC5E1A5),
+  "blue": const Color(0xFFBBDEFB),
+  "pink": const Color(0xFFFFF59D),
+};
