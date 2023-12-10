f81b58555e00c218b59e3347e155e6aea63f1ca2
DiegoDefSan <defsan35@gmail.com>
2023-12-10 10:02:21 -0300

    feat: user controller added

---
+++ projeto_final/lib/controllers/User.controller.dart
@@ -0,0 +1,16 @@
+import 'package:projeto_final/models/user/User.dart';
+import 'package:projeto_final/models/user/User.dto.credentials.dart';
+import 'package:projeto_final/models/user/User.dto.newUser.dart';
+import 'package:projeto_final/services/User.service.dart';
+
+class UserController {
+  final UserService _userService = UserService();
+
+  Future<User?> loginUser(UserDtoCredentials userDtoCredentials) {
+    return _userService.authenticateUser(userDtoCredentials);
+  }
+
+  Future<bool> registerUser(UserDtoNewUser userDtoNewUser) {
+    return _userService.registerUser(userDtoNewUser);
+  }
+}
