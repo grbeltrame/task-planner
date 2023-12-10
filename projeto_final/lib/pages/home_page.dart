1f2f8059e00ca76b6be7d7323ab5f9b90ae0f5fe
DiegoDefSan <defsan35@gmail.com>
2023-12-10 10:04:23 -0300

    ref: homepage refactored

--- projeto_final/lib/pages/home_page.dart
+++ projeto_final/lib/pages/home_page.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_final/controllers/User.controller.dart';
+import 'package:projeto_final/pages/widgets/login_form.dart';
+import 'package:projeto_final/pages/widgets/register_form.dart';
 import '/pages/dashboard_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final UserController _userController = UserController();
+
+  bool onLoginForm = true;
+
+  void changeWidgetForm() {
+    setState(() {
+      onLoginForm = !onLoginForm;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,56 +30,65 @@ class HomePage extends StatelessWidget {
 
       backgroundColor: Colors.black87, // Alterado para um preto não tão preto
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                // Implementar lógica de login
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => DashboardPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue, // Cor do botão de login
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(20.0), // Borda arredondada
-                ),
-                minimumSize: Size(200, 50), // Tamanho do botão
+        child: onLoginForm
+            ? LoginForm(
+                changeWidgetForm: changeWidgetForm,
+                userController: _userController,
+              )
+            : RegisterForm(
+                changeWidgetForm: changeWidgetForm,
+                userController: _userController,
               ),
-              child: Text(
-                'Login',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-            ),
-            SizedBox(height: 20.0), // Distância vertical entre os botões
-            ElevatedButton(
-              onPressed: () {
-                // Implementar tela de cadastro
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.transparent, // Cor de fundo transparente
-                side: BorderSide(
-                    color: Colors.blue), // Cor da borda do botão de cadastro
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(20.0), // Borda arredondada
-                ),
-                minimumSize: Size(200, 50), // Tamanho do botão
-              ),
-              child: Text(
-                'Cadastrar',
-                style: TextStyle(fontSize: 18.0, color: Colors.blue),
-              ),
-            ),
-          ],
-        ),
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: <Widget>[
+        //       ElevatedButton(
+        //         onPressed: () {
+        //           // Implementar lógica de login
+        //           Navigator.push(
+        //             context,
+        //             MaterialPageRoute(builder: (context) => DashboardPage()),
+        //           );
+        //         },
+        //         style: ElevatedButton.styleFrom(
+        //           backgroundColor: Colors.blue, // Cor do botão de login
+        //           shape: RoundedRectangleBorder(
+        //             borderRadius:
+        //                 BorderRadius.circular(20.0), // Borda arredondada
+        //           ),
+        //           minimumSize: Size(200, 50), // Tamanho do botão
+        //         ),
+        //         child: Text(
+        //           'Login',
+        //           style: TextStyle(
+        //               fontSize: 20.0,
+        //               fontWeight: FontWeight.bold,
+        //               color: Colors.black),
+        //         ),
+        //       ),
+        //       SizedBox(height: 20.0), // Distância vertical entre os botões
+        //       ElevatedButton(
+        //         onPressed: () {
+        //           // Implementar tela de cadastro
+        //         },
+        //         style: ElevatedButton.styleFrom(
+        //           backgroundColor:
+        //               Colors.transparent, // Cor de fundo transparente
+        //           side: BorderSide(
+        //               color: Colors.blue), // Cor da borda do botão de cadastro
+        //           shape: RoundedRectangleBorder(
+        //             borderRadius:
+        //                 BorderRadius.circular(20.0), // Borda arredondada
+        //           ),
+        //           minimumSize: Size(200, 50), // Tamanho do botão
+        //         ),
+        //         child: Text(
+        //           'Cadastrar',
+        //           style: TextStyle(fontSize: 18.0, color: Colors.blue),
+        //         ),
+        //       ),
+        //     ],
+        //   ),
       ),
     );
   }
